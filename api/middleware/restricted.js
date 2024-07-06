aafbc9df1210fcf673b5a994f97bc00031a38e04
SilentBN <61912362+SilentBN@users.noreply.github.com>
2024-07-06 15:21:16 -0400

    Implement restricted middleware for protected routes

--- api/middleware/restricted.js
+++ api/middleware/restricted.js
@@ -1,5 +1,8 @@
+const jwt = require("jsonwebtoken");
+
+const JWT_SECRET = process.env.JWT_SECRET || "Keep it secret, keep it safe!";
+
 module.exports = (req, res, next) => {
-  next();
   /*
     IMPLEMENT
 
@@ -11,4 +14,18 @@ module.exports = (req, res, next) => {
     3- On invalid or expired token in the Authorization header,
       the response body should include a string exactly as follows: "token invalid".
   */
+  const token = req.headers.authorization;
+
+  if (!token) {
+    return res.status(401).json({ message: "token required" });
+  }
+
+  jwt.verify(token, JWT_SECRET, (err, decodedToken) => {
+    if (err) {
+      return res.status(401).json({ message: "token invalid" });
+    }
+
+    req.decodedToken = decodedToken;
+    next();
+  });
 };
